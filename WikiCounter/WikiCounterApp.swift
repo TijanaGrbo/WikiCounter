6aa4158cad045ea5179b4d021b53ddf6eb322b77
Tijana Grbo <tijana.grbo@gmail.com>
2024-10-14 12:18:47 +0200

    Initial Commit

---
+++ WikiCounter/WikiCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiCounterApp.swift
+//  WikiCounter
+//
+//  Created by Tijana Grbo on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WikiCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
